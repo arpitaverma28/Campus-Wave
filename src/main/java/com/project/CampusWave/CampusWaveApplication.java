23973d18409f5636f7d0420d53224d1cd4d9eea2
arpitaverma28 <arpitaverma13578@gmail.com>
2026-04-17 21:14:43 +0530

    first commit

---
+++ src/main/java/com/project/CampusWave/CampusWaveApplication.java
@@ -0,0 +1,13 @@
+package com.project.CampusWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CampusWave/CampusWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CampusWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
